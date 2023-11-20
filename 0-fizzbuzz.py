f8dc6205165a1893a0e903157ebcf126d77b6078
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-20 10:47:29 +0300

    0-fizzbuzz.py

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ FizzBuzz
 """
-import sys
-
 
 def fizzbuzz(n):
     """
@@ -15,25 +13,12 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0:
-            tmp_result.append("Fizz")
+           print("Fizz end="")
         elif (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
+            print"FizzBuzz end=""
         elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+            print("Buzz end="")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
-
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+            print "i"
